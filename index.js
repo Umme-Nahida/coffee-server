f9ed8ea83e4973cfaa846ea3e63af36d1b229345
Umme-Nahida <mysuccessmission@gmail.com>
2024-07-07 21:16:30 +0600

    some changes

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+var cors = require('cors')
 const app = express()
 const port = 5000
 
+// middleware
+app.use(cors())
+app.use(express.json())
 
 
-console.log(process.env.DB_PASS);
-console.log(process.env.DB_USER);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ytj0kf8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,9 +25,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("insertDB");
+    const coffeeCollection = database.collection("coffeeCollection")
+  
+    // create a new coffee
+    app.post('/addcoffee',async(req,res)=>{
+      const coffee = req.body;
+      console.log(coffee)
+      const result = await coffeeCollection.insertOne(coffee);
+      res.send(result);
+    })
 
-
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
